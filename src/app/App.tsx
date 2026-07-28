3f285c67673f741bddf2ff76fa8a9a270e124462
thwjddlqslek <20211046@sungshin.ac.kr>
2026-07-28 23:14:23 +0900

    feat: Tanstack Query 세팅
    shared/config에 QueryClient를 구성하고 App 엔트리를
    QueryClientProvider로 감싼다.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,16 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { HomePage } from "@/pages/home";
+import { queryClient } from "@/shared/config/query-client";
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
