84273586671f0227c773035b11db963920159dc7
sohxxny <sohxxny@gmail.com>
2026-08-18 21:57:10 +0900

    refactor: 운동 경력 선택 UI를 ExperienceLevelOptions로 분리
    useOnboardingForm() 컨텍스트에 묶여있던 옵션 목록 UI를 value/onChange
    prop만 받는 순수 컴포넌트로 뽑아서, 온보딩 바깥(프로필 편집)에서도
    재사용할 수 있게 했다. ExperienceLevelStep은 이 컴포넌트를 쓰도록 리팩터.

--- src/features/onboarding-form/steps/ExperienceLevelStep.tsx
+++ src/features/onboarding-form/steps/ExperienceLevelStep.tsx
@@ -1,7 +1,6 @@
-import { Button } from "@/shared/ui/button";
-import { EXPERIENCE_LEVEL_OPTIONS } from "../constants/form-fields";
 import { useOnboardingForm } from "../model/use-onboarding-form";
 import StepLayout from "../components/StepLayout";
+import ExperienceLevelOptions from "../components/ExperienceLevelOptions";
 
 export default function ExperienceLevelStep() {
   const {
@@ -11,19 +10,7 @@ export default function ExperienceLevelStep() {
 
   return (
     <StepLayout title="운동을 하신지 얼마나 됐나요?">
-      <div className="flex flex-col gap-[1.2rem]">
-        {EXPERIENCE_LEVEL_OPTIONS.map((option) => (
-          <Button
-            key={option.value}
-            color="secondary"
-            isSelected={experienceLevel === option.value}
-            onClick={() => updateExperienceLevel(option.value)}
-            className="py-[2.2rem] px-[4.1rem]"
-          >
-            {option.label}
-          </Button>
-        ))}
-      </div>
+      <ExperienceLevelOptions value={experienceLevel} onChange={updateExperienceLevel} />
     </StepLayout>
   );
 }
